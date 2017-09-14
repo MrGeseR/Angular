95517b92d8e029a4c352dad9570cbb492c72f568
JDev <dev@temposys.name>
2017-09-14 16:02:39 +0300

    Base structure of a project created

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+      new Recipe('Test', 'This is a simple test recipe', 'https://c1.staticflickr.com/1/501/31577489463_1e37b95427_b.jpg'),
+      new Recipe('Test', 'This is a simple test recipe', 'https://c1.staticflickr.com/1/501/31577489463_1e37b95427_b.jpg'),
+  ];
 
   constructor() { }
 
