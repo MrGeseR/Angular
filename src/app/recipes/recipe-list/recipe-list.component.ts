f6ca38f5e77c8a9e0b9b827a4287e65fb2a56347
JDev <dev@temposys.name>
2017-09-18 11:13:12 +0300

    Something was added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
       new Recipe('Test', 'This is a simple test recipe', 'https://c1.staticflickr.com/1/501/31577489463_1e37b95427_b.jpg'),
-      new Recipe('Test', 'This is a simple test recipe', 'https://c1.staticflickr.com/1/501/31577489463_1e37b95427_b.jpg'),
+      new Recipe('Another Recipe', 'This is a simple test recipe', 'https://c1.staticflickr.com/1/501/31577489463_1e37b95427_b.jpg'),
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
