daff9aa3acdaff75d62e1d6a65511967f39ca8a4
JDev <dev@temposys.name>
2017-10-02 17:28:25 +0300

    Finished with routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,15 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
   }
-  onSelect(feature: string){
-    this.featureSelected.emit(feature);
-  }
+
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
@@ -25,10 +24,18 @@ export class RecipeService {
             ])
     ];
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
       return this.recipes.slice();
+  }
+
+  getRecipe(id: number) {
+      return this.recipes[id];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+      this.slService.addIngredients(ingredients);
   }
 
 }
